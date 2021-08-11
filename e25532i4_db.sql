e4bb860382959483a5b42abbabe89ed52e2b23d7
rudenokart <rudenokart@yandex.ru>
2021-08-11 19:57:04 +0300

    20210811

--- e25532i4_db.sql
+++ e25532i4_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июл 03 2021 г., 07:57
+-- Время создания: Апр 29 2021 г., 23:13
 -- Версия сервера: 5.6.47
--- Версия PHP: 7.2.29
+-- Версия PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,23 +42,15 @@ CREATE TABLE `festival_meta` (
 INSERT INTO `festival_meta` (`id`, `master`, `cd`, `orchestra`, `food`) VALUES
 (33, 'mk 1', '', '', ''),
 (34, 'mk 2', '', '', ''),
-(35, '1 окт. 14.30-16.00 Рогова А.', '', '', ''),
+(35, 'mk 3', '', '', ''),
 (36, '', 'cd 1', '', ''),
 (37, '', 'cd 2', '', ''),
 (38, '', 'cd 3', '', ''),
 (39, '', '', 'orc 1', ''),
 (40, '', '', 'orc 2', ''),
-(41, '', '', 'Корона МираМар', ''),
+(41, '', '', 'orc 3', ''),
 (42, '', '', '', 'dinner 1'),
-(44, '', '', '', 'dinner 2'),
-(45, '', '', 'Королева Импровизации', ''),
-(46, '', 'Египетский фольклор СД ', '', ''),
-(47, 'mk 4', '', '', ''),
-(48, 'мк 5', '', '', ''),
-(49, 'мк 6', '', '', ''),
-(50, 'мк 7', '', '', ''),
-(51, 'мк 8', '', '', ''),
-(52, 'мк 9', '', '', '');
+(44, '', '', '', 'dinner 2');
 
 -- --------------------------------------------------------
 
@@ -91,76 +83,9 @@ CREATE TABLE `festival_participant` (
 --
 
 INSERT INTO `festival_participant` (`id`, `password`, `email`, `fio`, `date`, `phone`, `country`, `city`, `age`, `skill`, `studio`, `manager`, `discount`, `package`, `photo`, `certificate`, `coment`) VALUES
-(14, 'пас1', 'лог1', 'Вася Пряников', '01.01.1990', '+7(333) 333-3333', 'Страна', 'Город', 'Взрослые 1 (20-30 лет)', 'Любители продолжающие', 'Студия', 'Руководитель', 20, 'maxi', 'photo!!14!!Вася Пряников.png', 'certificate!!14!!Вася Пряников.png', 'комментарий 1\r\nс новой строки\r\n3-я строка'),
-(15, 'пас2', 'лог2', 'лог2', '01.05.2021', '', '', '', 'Дети (7-10лет)', 'Профессионалы', '', '', 0, 'mini', '', '', '');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `festival_payment`
---
-
-CREATE TABLE `festival_payment` (
-  `id` int(10) NOT NULL,
-  `date` date NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `amount` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `festival_payment`
---
-
-INSERT INTO `festival_payment` (`id`, `date`, `user_id`, `amount`) VALUES
-(4, '2021-05-01', 14, 500),
-(7, '2021-03-02', 14, 1000),
-(8, '2021-05-06', 15, 5000);
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `festival_price`
---
-
-CREATE TABLE `festival_price` (
-  `id` int(10) NOT NULL,
-  `date` date NOT NULL,
-  `meta_type` varchar(250) NOT NULL,
-  `meta` varchar(250) NOT NULL,
-  `price` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `festival_price`
---
-
-INSERT INTO `festival_price` (`id`, `date`, `meta_type`, `meta`, `price`) VALUES
-(510, '2021-05-02', 'master', '33', 2000),
-(511, '2021-05-02', 'master', '34', 2000),
-(512, '2021-05-02', 'master', '35', 2000),
-(513, '2021-05-02', 'master', '47', 2000),
-(514, '2021-05-02', 'master', '48', 2000),
-(515, '2021-05-02', 'master', '49', 2000),
-(516, '2021-05-02', 'master', '50', 2000),
-(517, '2021-05-02', 'master', '51', 2000),
-(518, '2021-05-02', 'master', '52', 2000),
-(519, '2021-05-02', 'cd', '36', 2000),
-(520, '2021-05-02', 'cd', '37', 2000),
-(521, '2021-05-02', 'cd', '38', 2000),
-(522, '2021-05-02', 'cd', '46', 2000),
-(523, '2021-05-02', 'orchestra', '39', 2000),
-(524, '2021-05-02', 'orchestra', '40', 2000),
-(525, '2021-05-02', 'orchestra', '41', 2000),
-(526, '2021-05-02', 'orchestra', '45', 2000),
-(527, '2021-05-02', 'food', '42', 2000),
-(528, '2021-05-02', 'food', '44', 2000),
-(529, '2021-05-02', 'package', 'mini', 2000),
-(530, '2021-05-02', 'package', 'midi', 2000),
-(531, '2021-05-02', 'package', 'maxi', 2000),
-(532, '2021-05-02', 'package', 'junior', 2000),
-(533, '2021-05-02', 'package', 'study', 2000),
-(534, '2021-05-02', 'package', 'all_ws', 2000),
-(535, '2021-05-02', 'package', 'group', 0);
+(14, 'пас1', 'лог1', 'ФИО', '01.01.2000', '+7(333) 333-3333', 'Страна', 'Город', 'Взрослые 1 (20-30 лет)', 'Любители продолжающие', 'Студия', 'Руководитель', 20, 'junior', 'photo!!14!!ФИО.png', 'certificate!!14!!ФИО.png', 'комент'),
+(16, 'пас3', 'лог3', 'фио', '26.04.2021', '+7(111) 111-1111', 'РФ', 'СПБ', 'возр', 'Любители продолжающие', 'студ', 'рук', 0, '', '1619465272.png', 'certificate!!16!!фио.png', ''),
+(58, 'art', 'RudenokArt@yandex.ru', '', '', '', '', '', '', '', '', '', 0, '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -194,23 +119,11 @@ INSERT INTO `festival_user_category` (`user`, `category`, `orchestra`) VALUES
 (57, 41, 'Композиция для orc 1 '),
 (57, 42, ''),
 (57, 44, ''),
-(59, 35, ''),
-(59, 38, ''),
-(59, 40, '<br /><b>Notice</b>:  Undefined offset: 0 in <b>D:OpenServerdomainslocalhost20210421-festivaluser.php</b> on line <b>464</b><br />'),
-(59, 44, ''),
-(15, 33, ''),
-(15, 36, ''),
-(15, 39, '<br /><b>Notice</b>:  Undefined offset: 0 in <b>D:OpenServerdomainslocalhost20210421-festivalincludesuser_files.php</b> on line <b>100</b><br />'),
-(15, 42, ''),
-(14, 34, ''),
+(14, 33, ''),
 (14, 35, ''),
-(14, 47, ''),
-(14, 48, ''),
-(14, 49, ''),
-(14, 50, ''),
-(14, 51, ''),
-(14, 52, ''),
-(14, 40, '<br /><b>Notice</b>:  Undefined offset: 0 in <b>D:OpenServerdomainslocalhost20210421-festivaluser.php</b> on line <b>464</b><br />');
+(14, 39, 'Композиция'),
+(14, 41, 'Композиция'),
+(14, 42, '');
 
 --
 -- Индексы сохранённых таблиц
@@ -229,18 +142,6 @@ ALTER TABLE `festival_participant`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `festival_payment`
---
-ALTER TABLE `festival_payment`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `festival_price`
---
-ALTER TABLE `festival_price`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -248,25 +149,13 @@ ALTER TABLE `festival_price`
 -- AUTO_INCREMENT для таблицы `festival_meta`
 --
 ALTER TABLE `festival_meta`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT для таблицы `festival_participant`
 --
 ALTER TABLE `festival_participant`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT для таблицы `festival_payment`
---
-ALTER TABLE `festival_payment`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT для таблицы `festival_price`
---
-ALTER TABLE `festival_price`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=536;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
